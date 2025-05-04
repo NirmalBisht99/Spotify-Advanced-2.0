fe142e6aa6a43fef3e028d539db5d56c628d8d2a
NirmalBisht99 <nirmalbisht2599@gmail.com>
2025-05-04 12:22:54 +0530

    Done up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+function App() {
+  return (
+    <>
+      <header>
+      <SignedOut>
+        <SignInButton >
+          <Button >Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  );
+}
+
+export default App
