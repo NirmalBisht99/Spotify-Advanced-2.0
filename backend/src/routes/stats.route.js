05fcb8b3c2491d9b330cbb49ed32c8a8efec6677
NirmalBisht99 <nirmalbisht2599@gmail.com>
2025-05-04 12:21:26 +0530

    set up frontend backend and databse

---
+++ backend/src/routes/stats.route.js
@@ -0,0 +1,12 @@
+
+  import { Router } from 'express';
+  
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.send('Stats route is working!');
+}
+)
+  
+  export default router;
+  
