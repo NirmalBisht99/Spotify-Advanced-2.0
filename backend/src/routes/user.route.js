05c2dae7b3601a589fe18f3f530ca4917d6f58ee
NirmalBisht99 <nirmalbisht2599@gmail.com>
2025-05-05 18:19:14 +0530

    all set

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,30 @@
-import { Router } from 'express';
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { getAllUsers } from '../controller/user.controller.js';
-  const router = Router();
+import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js";
 
-	router.get('/' ,protectRoute , getAllUsers);
+export const getAllUsers = async (req, res, next) => {
+	try {
+		const currentUserId = req.auth.userId;
+		const users = await User.find({ clerkId: { $ne: currentUserId } });
+		res.status(200).json(users);
+	} catch (error) {
+		next(error);
+	}
+};
 
+export const getMessages = async (req, res, next) => {
+	try {
+		const myId = req.auth.userId;
+		const { userId } = req.params;
 
-  export default router;
-  
+		const messages = await Message.find({
+			$or: [
+				{ senderId: userId, receiverId: myId },
+				{ senderId: myId, receiverId: userId },
+			],
+		}).sort({ createdAt: 1 });
+
+		res.status(200).json(messages);
+	} catch (error) {
+		next(error);
+	}
+};
